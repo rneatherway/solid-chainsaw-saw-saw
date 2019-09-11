b4e6c29dbbffba03a6727f057d81f96fdcbafdbb
Robin Neatherway <robin.neatherway@gmail.com>
2019-09-11 11:49:23 +0100

    Update main.c

--- main.c
+++ main.c
@@ -8,16 +8,7 @@ void bar(char* s) {
 void foo(char* s) {
     bar(s);
 }
-
-void usage(int argc, char* self) {
-    printf("Error: Wrong number of arguments (%d)\n", argc - 1);
-    printf("Usage: %s <string>\n", self);
-}
-
-int main(int argc, char* argv[]) {
-
-    if (argc != 2) {
-        usage(argc, argv[0]);
+;
         exit(1);
     }
 
